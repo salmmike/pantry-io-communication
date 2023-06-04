10aa917674579dc55e35e387465f7f802fff2443
Mike Salmela <mike.salmela@iki.fi>
2023-06-04 14:29:48 +0300

    Add signal that indicates data has changed

--- src/main.c
+++ src/main.c
@@ -2,16 +2,55 @@
 #include <glib.h>
 #include <gio/gio.h>
 
+
+#define OBJECT_PATH "/org/gtk/GDBus/PantryIOCommunicator"
+#define INTERFACE_NAME "org.pantryio.GDBus.StatusInterface"
+#define DATA_CHANGED_SIGNAL "PantryDataChanged"
+#define GET_STATUS_METHOD "GetStatus"
+#define UNIX_ADDRESS "unix:abstract=pantryiocommunication"
+
 static GDBusNodeInfo *introspection_data = NULL;
 
 static const gchar introspection_xml[] =
     "<node>"
-    "  <interface name='org.pantryio.GDBus.StatusInterface'>"
-    "    <method name='GetStatus'>"
+    "  <interface name='" INTERFACE_NAME "'>"
+    "    <method name='" GET_STATUS_METHOD "'>"
     "      <arg type='s' name='currentData' direction='out'/>"
     "    </method>"
+    "    <signal name='" DATA_CHANGED_SIGNAL "'>"
+    "      <arg type='s' name='currentData' direction='out'/>"
+    "    </signal>"
     "  </interface>"
     "</node>";
+
+
+void emit_signal(void)
+{
+        gboolean ret;
+        GDBusConnection* connection;
+        GError* error;
+        error = NULL;
+
+        connection = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
+        if (error != NULL) {
+            g_printerr("Failed to get bus connection: %s\n", error->message);
+            g_error_free(error);
+            return;
+        }
+
+        ret = g_dbus_connection_emit_signal (connection,
+                                             NULL,
+                                             OBJECT_PATH,
+                                             INTERFACE_NAME,
+                                             DATA_CHANGED_SIGNAL,
+                                             NULL,
+                                             &error);
+        if (error != NULL) {
+            printf("Try print error\n");
+            g_printerr ("Error?: %s\n", error->message);
+            g_error_free(error);
+        }
+}
 
 static void
 handle_method_call (G_GNUC_UNUSED
@@ -25,10 +64,15 @@ handle_method_call (G_GNUC_UNUSED
                     GDBusMethodInvocation *invocation,
                     gpointer               user_data)
 {
+    GError *error;
+    error = NULL;
+    gboolean ret;
+
     if (g_strcmp0(method_name, "GetStatus") == 0) {
-        gchar *response = "abc=1,123=0,cdef=0";
+        gchar *response = "abc=1,123=0,cdef=0,longstring item with space=1";
         g_dbus_method_invocation_return_value(invocation,
                                               g_variant_new("(s)", response));
+        emit_signal();
     }
 }
 
@@ -59,8 +103,8 @@ on_new_connection (GDBusServer *server,
     guint registration_id;
     g_object_ref (connection);
     g_signal_connect (connection, "closed", G_CALLBACK (connection_closed), NULL);
-    registration_id = g_dbus_connection_register_object (connection,
-                                                        "/org/gtk/GDBus/PantryIOCommunicator",
+    registration_id = g_dbus_connection_register_object(connection,
+                                                        OBJECT_PATH,
                                                         introspection_data->interfaces[0],
                                                         &interface_vtable,
                                                         NULL,  /* user_data */
@@ -115,7 +159,7 @@ authorize_authenticated_peer_cb (GDBusAuthObserver *observer,
 int main()
 {
     gint ret;
-    gchar *opt_address = "unix:abstract=pantryiocommunication";
+    gchar *opt_address = UNIX_ADDRESS;
     GError *error;
 
     introspection_data = g_dbus_node_info_new_for_xml(introspection_xml, &error);
@@ -127,7 +171,7 @@ int main()
     GMainLoop *loop;
     GDBusServerFlags server_flags;
 
-    guid = g_dbus_generate_guid ();
+    guid = g_dbus_generate_guid();
 
     server_flags = G_DBUS_SERVER_FLAGS_NONE;
 
@@ -136,7 +180,7 @@ int main()
     g_signal_connect (observer, "authorize-authenticated-peer", G_CALLBACK (authorize_authenticated_peer_cb), NULL);
 
     error = NULL;
-    server = g_dbus_server_new_sync (opt_address,
+    server = g_dbus_server_new_sync(opt_address,
                                     server_flags,
                                     guid,
                                     observer,
